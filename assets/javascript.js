d7b8f1ad42d037a46a953030be1e52c3cb719c60
ABNER TORIBIO <abnertoribio@live.com>
2021-04-11 16:06:29 -0400

    adjusted the alignment on the side section

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,10 +1,9 @@
-$(document).scroll(function() {
-    var y = $(this).scrollTop();
-    if (y > 750) {
-      $('#sideSection').fadeIn();
-    } else {
-      $('#sideSection').fadeOut();
-    }
-  });
 
-  $("#project1").attr("href", "https://www.linkedin.com/in/abner-toribio-vazquez-726241127/")
+$(document).scroll(function () {
+    var y = $(this).scrollTop();
+     if (y > 750) {
+        $('#sideSection').fadeIn();
+    } else {
+        $('#sideSection').fadeOut();
+    }
+});
